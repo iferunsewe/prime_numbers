e21798521b9f0745c8c02f54ad0c83f2436f21e6
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-08 11:15:09 +0000

    more specs

--- spec/prime_numbers_table_spec.rb
+++ spec/prime_numbers_table_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 RSpec.describe PrimeNumbersTable do
   subject(:create_prime_numbers_table) { described_class.new(number) }
+  let(:arr_of_prime_numbers){ Prime.first(number) }
+  let(:table){  create_prime_numbers_table.table  }
 
   context 'when you do not provide an integer' do
     let(:number) { 'X' }
@@ -11,4 +13,34 @@ RSpec.describe PrimeNumbersTable do
     end
   end
 
+  context '#build' do
+    let(:number) { 10 }
+
+    it 'returns a table' do
+      expect(table).to be_kind_of(Terminal::Table)
+    end
+
+    it 'returns a number of columns which is the number of prime numbers plus 1 for column headings' do
+      expect(table.number_of_columns).to eq number + 1
+    end
+
+    it 'returns a number of rows which is the number of prime numbers' do
+      expect(table.rows.count).to eq number
+    end
+
+    it 'returns a first column of all n original prime numbers' do
+      expect(table.columns[0]).to eq arr_of_prime_numbers
+    end
+
+    it 'returns a table where the cells are the result of multiplying two prime numbers' do
+      p_n1 = arr_of_prime_numbers.sample
+      p_n2 = arr_of_prime_numbers.sample
+      original_n_prime_numbers = table.columns[0]
+      index_of_p_n1_number = original_n_prime_numbers.find_index(p_n1)
+      index_of_p_n2_number = original_n_prime_numbers.find_index(p_n2)
+      expect(
+        table.columns[index_of_p_n1_number + 1][index_of_p_n2_number]
+      ).to eq p_n1 * p_n2
+    end
+  end
 end
