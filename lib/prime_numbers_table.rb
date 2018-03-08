d6f2b2ba2ad8db2df6af123741063809904e11e4
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-08 11:15:43 +0000

    implementation of table

--- lib/prime_numbers_table.rb
+++ lib/prime_numbers_table.rb
@@ -10,6 +10,34 @@ class PrimeNumbersTable
     raise ArgumentError, "#{number} is not an integer" unless number.is_a? Integer
     @number = number.to_i
     @table = Terminal::Table.new
+    build
   end
 
+  def build
+    table.rows = build_rows_with_column_headings
+    table.headings = build_headings
+    table
+  end
+
+  private
+
+  def get_n_prime_numbers
+    Prime.first(@number)
+  end
+
+  def build_rows
+    get_n_prime_numbers.map do |p|
+      get_n_prime_numbers.collect { |n| n * p if p.is_a? Integer }
+    end
+  end
+
+  def build_headings
+    get_n_prime_numbers.unshift('')
+  end
+
+  def build_rows_with_column_headings
+    build_rows.map.with_index do |row, i|
+      row.unshift(get_n_prime_numbers[i])
+    end
+  end
 end
